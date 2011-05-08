439354d4654629c9ff9911788f61770b2155dfee
Ben Fields <b.fields@gold.ac.uk>
2011-05-08 13:29:26 +0100

    handles unicode properly now

--- firealarm.py
+++ firealarm.py
@@ -15,17 +15,18 @@ import sys
 import os
 import urllib2
 import re
+import logging
 from simplejson import loads
 log = logging.getLogger(__name__)
 
-from apikey_private import *
+from apikey import *
 
 
 def process_exquisite(raw_html):
 	'''
 	takes in html of the sort found from an equisite tweet conversation and returns
 	'''
-	return re.findall('http://twitter.com/[\w]*/status/([\d]*)">[\d]', raw_data)
+	return re.findall('http://twitter.com/[\w]*/status/([\d]*)">[\d]', raw_html)
 	
 def determine_tweet_sentiment(status_id):
 	'''
@@ -33,10 +34,12 @@ def determine_tweet_sentiment(status_id):
 	returns the sentiment of the tweet, 1 is very negative, 5 is very positive
 	'''
 	data = loads(urllib2.urlopen("http://api.twitter.com/1/statuses/show/{0}.json".\
-				format(twitter_id)).read())
-	tweet_content = data["text"]
-	return loads(urllib2.urlopen("http://apib1.semetric.com/musicmetric/sentiment?token="+\
-					API_KEY, data = tweet_content).read())
+				format(status_id)).read())
+	tweet_content = data["text"].encode('utf-8')
+	score =  int(loads(urllib2.urlopen("http://apib1.semetric.com/musicmetric/sentiment?token="+\
+					API_KEY, data = tweet_content).read())['score'])
+	log.debug( (data["text"]) +  "::" +  str(score))
+	return score
 	
 def find_max_neg_delta(senti_list):
 	'''
@@ -44,41 +47,48 @@ def find_max_neg_delta(senti_list):
 	returns the index
 	'''
 	max_neg_delta = 0
-	last_senti = -1
+	last_senti = senti_list[0]
 	winner_idx = None
-	for idx, current_senti in enumerate(senti_list):
+	for idx, current_senti in enumerate(senti_list[1:]):
 		if current_senti - last_senti < max_neg_delta:
 			max_neg_delta = current_senti
 			winner_idx = idx
+		last_senti = current_senti
 	return winner_idx
 
 def main(argv):
 	print 'fetching the conversation at', argv[1]
 	raw_data = urllib2.urlopen(argv[1]).read()
 	status_IDs = process_exquisite(raw_data)
+	status_IDs.reverse() #flip them to be in written order
 	
-	print 'Is the conversation a flame war?',
-	sentiments = maps(lambda x:determine_tweet_sentiment(x), status_IDs)
+	print 'Is the conversation a flame war?'
+	sentiments = map(lambda x:determine_tweet_sentiment(x), status_IDs)
 	mean_sent = reduce(lambda x,y:x+y, sentiments)/float(len(sentiments))
 	if mean_sent < 3:
-		print "yes"
+		print "\tyes"
 	elif mean_sent > 3:
-		print "no"
+		print "\tno"
 	else:
-		print "maybe"
+		print "\tmaybe"
 	
 	print "Where's the troll?"
 	winner_idx = find_max_neg_delta(sentiments)
-	print '\t', re.findall('(http://twitter.com/[\w]*/status/'+\
-			str(status_IDs[winner_idx])+')">[\d]', raw_data)
+	try:
+		print '\t', re.findall('(http://twitter.com/[\w]*/status/'+\
+				str(status_IDs[winner_idx])+')">[\d]', raw_data)[0]
+	except TypeError:
+		print "no troll, this conversation was flat."
+		return 0
+		
 	data = loads(urllib2.urlopen("http://api.twitter.com/1/statuses/show/{0}.json".\
 				format(status_IDs[winner_idx])).read())
-	print "Author:", data['user']['name'], 'username:'data['user']['screen_name']
+	print "Author:", data['user']['name'], 'username:', data['user']['screen_name']
 	print "Now stop feeding the trolls!"
-
-
+	return 0
+	
 if __name__ == '__main__':
-	logging.basicConfig(level=logging.DEBUG, 
+	logging.basicConfig(level=logging.INFO, 
 		format='%(asctime)s %(levelname)s %(message)s')
 	sys.exit(main(sys.argv))
 
